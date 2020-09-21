6af81e7295803cd2828b8218b15c18b9d3d7709d
gyeongwon1275 <rlaruddnjs28@gmail.com>
2020-09-21 19:58:22 +0900

    :lipstick: Add reponsive Nav

--- src/components/Tool/Header/Nav.tsx
+++ src/components/Tool/Header/Nav.tsx
@@ -5,6 +5,10 @@ import { Link, useLocation } from 'react-router-dom'
 function Nav() {
   const location = useLocation()
 
+  const [isOpenSideMenu, openSideMenu] = React.useState(false)
+
+  const closeSideMenu = () => openSideMenu(false)
+  const showSideMenu = () => openSideMenu(true)
   return (
     <header>
       <div className="tool-nav-container">
@@ -52,6 +56,57 @@ function Nav() {
             나이 계산
           </Link>
         </div>
+
+        <nav className={isOpenSideMenu ? 'shown-nav' : 'hidden-nav'}>
+          <i className="fas fa-times cancel-icon" onClick={closeSideMenu} />
+          <div className="hidden-nav-menu">
+            <Link
+              to="/tool"
+              onClick={closeSideMenu}
+              className={location.pathname === '/tool' ? 'current-nav' : ''}
+            >
+              홈
+            </Link>
+            <Link
+              to="/tool/foodCalorie"
+              onClick={closeSideMenu}
+              className={
+                location.pathname === '/tool/foodCalorie' ? 'current-nav' : ''
+              }
+            >
+              사료칼로리 계산
+            </Link>
+            <Link
+              to="/tool/recommendCalorie"
+              onClick={closeSideMenu}
+              className={
+                location.pathname === '/tool/recommendCalorie'
+                  ? 'current-nav'
+                  : ''
+              }
+            >
+              권장칼로리 계산
+            </Link>
+            <Link
+              to="/tool/bmi"
+              onClick={closeSideMenu}
+              className={location.pathname === '/tool/bmi' ? 'current-nav' : ''}
+            >
+              비만도 계산
+            </Link>
+            <Link
+              to="/tool/age"
+              onClick={closeSideMenu}
+              className={location.pathname === '/tool/age' ? 'current-nav' : ''}
+            >
+              나이 계산
+            </Link>
+          </div>
+          <hr />
+        </nav>
+
+        <i className="fas fa-bars nav-menu-icon" onClick={showSideMenu}></i>
+        {/* <i className="fas fa-hamburger nav-hamburger"></i> */}
       </div>
     </header>
   )
